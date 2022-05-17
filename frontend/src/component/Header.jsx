e3542f1f0bd024173d57abaf484d6ce6324e5304
Fernando Sena <fernandosenacruz@gmail.com>
2022-05-17 09:50:50 -0300

    css ajusts

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import BANNER from "../assets/blitz.png";
+import React from 'react';
+import BANNER from '../assets/blitz.png';
 
 const Header = () => {
   return (
     <div className="banner-blitz">
-      <img src={BANNER} alt="Collage blitz img" />
+      <img src={BANNER} alt="Collage blitz img" className="blitz-bg" />
     </div>
   );
 };
